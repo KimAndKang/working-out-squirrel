ef2cd80ce2b1a612436e6514dedd82a7757ca6e9
KimChanJin97 <cjkimhello97@gmail.com>
2024-12-14 17:20:48 +0900

    chore: initial backend setting

---
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/WOSApplication.java
@@ -0,0 +1,13 @@
+package com.kimandkang.workingoutsquirrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WOSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WOSApplication.class, args);
+	}
+
+}
