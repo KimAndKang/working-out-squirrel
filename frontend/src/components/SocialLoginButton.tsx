10a58a4d8955c683ba17eb50f32aa09cc74fedbe
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2024-12-18 16:31:45 +0900

    feat: global css font

--- frontend/src/components/SocialLoginButton.tsx
+++ frontend/src/components/SocialLoginButton.tsx
@@ -1,12 +1,12 @@
 type Props = {
-  color: string;
+  bgColor: string;
   logoUrl: string;
   text: string;
 };
 
-export default function SocialLoginButton({ color, logoUrl, text }: Props) {
+export default function SocialLoginButton({ bgColor, logoUrl, text }: Props) {
   return (
-    <button className={`flex bg-[${color}] rounded-md py-4 px-6 gap-5`}>
+    <button className={`flex ${bgColor} rounded-md py-4 px-6 gap-5`}>
       <img src={logoUrl} />
       <p className='text-black font-bold'>{text}</p>
     </button>
