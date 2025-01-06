8f8dd9a40317bdaa280c78d5c5a2829a116ea774
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-06 14:37:43 +0900

    feat: 액세스 토큰 블랙리스트, 리프레시 토큰 불일치 열거형 상수 추가

--- backend/src/main/java/com/kimandkang/workingoutsquirrel/redis/exception/TokenExceptionInfo.java
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/redis/exception/TokenExceptionInfo.java
@@ -1,13 +1,16 @@
 package com.kimandkang.workingoutsquirrel.redis.exception;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import com.kimandkang.workingoutsquirrel.common.exception.ExceptionInfo;
 import org.springframework.http.HttpStatus;
 
 public enum TokenExceptionInfo implements ExceptionInfo {
 
-    NO_SAVE_REFRESH_TOKEN(INTERNAL_SERVER_ERROR, 3000, "리프레시 토큰이 저장되지 않았습니다."),
+    NO_REFRESH_TOKEN(INTERNAL_SERVER_ERROR, 3000, "리프레시 토큰이 저장되지 않았습니다."),
+    BLACKLISTED_ACCESS_TOKEN(UNAUTHORIZED, 3001, "블랙리스트 처리된 액세스 토큰입니다."),
+    NOT_EQUAL_REFRESH_TOKEN(UNAUTHORIZED, 3002, "리프레시 토큰이 일치하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
