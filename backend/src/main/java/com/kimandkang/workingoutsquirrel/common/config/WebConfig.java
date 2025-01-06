7d183fe926e051b73bdea999378046d69e776de6
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-06 14:52:29 +0900

    config: CORS, favicon 설정

---
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/common/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.kimandkang.workingoutsquirrel.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("/js/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/css/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/images/");
+    }
+
+}
