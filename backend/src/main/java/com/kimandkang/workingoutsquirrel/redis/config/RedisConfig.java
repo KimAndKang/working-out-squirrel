1869de875fd4a43ad7dc2d3ab84c1f2ebd51229f
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-06 14:36:26 +0900

    config: 토큰 키(userId) 직렬화 String -> Long 타입 변경

--- backend/src/main/java/com/kimandkang/workingoutsquirrel/redis/config/RedisConfig.java
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/redis/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.kimandkang.workingoutsquirrel.redis.config;
 
 import static org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP;
 
+import com.kimandkang.workingoutsquirrel.redis.domain.AccessToken;
 import com.kimandkang.workingoutsquirrel.redis.domain.RefreshToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -30,15 +32,28 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, RefreshToken> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, RefreshToken> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
+    public RedisTemplate<Long, RefreshToken> refreshTokenRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Long, RefreshToken> refreshTokenRedisTemplate = new RedisTemplate<>();
+        refreshTokenRedisTemplate.setConnectionFactory(connectionFactory);
 
-        redisTemplate.setKeySerializer(new StringRedisSerializer()); // userId
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // RefreshToken
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        refreshTokenRedisTemplate.setKeySerializer(new GenericToStringSerializer<>(Long.class)); // userId
+        refreshTokenRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // RefreshToken
+        refreshTokenRedisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Long.class));
+        refreshTokenRedisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-        return redisTemplate;
+        return refreshTokenRedisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<Long, AccessToken> accessTokenRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Long, AccessToken> accessTokenRedisTemplate = new RedisTemplate<>();
+        accessTokenRedisTemplate.setConnectionFactory(connectionFactory);
+
+        accessTokenRedisTemplate.setKeySerializer(new GenericToStringSerializer<>(Long.class)); // userId
+        accessTokenRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // AccessToken
+        accessTokenRedisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Long.class));
+        accessTokenRedisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return accessTokenRedisTemplate;
     }
 }
