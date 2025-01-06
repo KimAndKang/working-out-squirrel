b42b88934a98f0a74d2f404367b5d3403a70385b
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-06 14:35:31 +0900

    feat: 토큰 userId 필드 String -> Long 타입 변경

--- backend/src/main/java/com/kimandkang/workingoutsquirrel/redis/domain/AccessToken.java
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/redis/domain/AccessToken.java
@@ -1,12 +1,44 @@
 package com.kimandkang.workingoutsquirrel.redis.domain;
 
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
-@Builder
+@NoArgsConstructor
+@RedisHash(value = "access", timeToLive = 259200)
 public class AccessToken {
 
-    private String userId;
+    @Id
+    private Long userId;
+
+    @Indexed
     private String claims;
+
+    @Builder
+    public AccessToken(Long userId, String claims) {
+        this.userId = userId;
+        this.claims = claims;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AccessToken that = (AccessToken) o;
+        return Objects.equals(getClaims(), that.getClaims());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClaims());
+    }
 }
