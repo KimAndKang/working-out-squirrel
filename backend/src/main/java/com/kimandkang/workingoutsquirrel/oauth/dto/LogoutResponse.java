9522d8d6c9a8c2909fc0ea5a269be249f79be365
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-06 14:51:52 +0900

    feat: 로그아웃, 토큰 재발급 DTO 구현

---
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/oauth/dto/LogoutResponse.java
@@ -0,0 +1,6 @@
+package com.kimandkang.workingoutsquirrel.oauth.dto;
+
+public record LogoutResponse(
+        boolean isBlackListed
+) {
+}
---
+++ backend/src/main/java/com/kimandkang/workingoutsquirrel/oauth/dto/ReissueRequest.java
@@ -0,0 +1,6 @@
+package com.kimandkang.workingoutsquirrel.oauth.dto;
+
+public record ReissueRequest(
+        String refreshToken
+) {
+}
